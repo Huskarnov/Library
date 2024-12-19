964de1bb30f5344ffe43625df963eb95fc4ee541
Huskarnov <youcef.merad.batna2m@gmail.com>
2024-12-19 17:37:29 +0100

    v

--- javascript.js
+++ javascript.js
@@ -57,6 +57,7 @@ let cardCreator = function(){ //render the books
             bookGrid.removeChild(currentChild); //remove from the dom parent
                     };
             });
+            // 
             //--------------------------------------------------------
             readStatus.classList.add('readStatus');
             readStatus.innerHTML ='<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m9 20.42l-6.21-6.21l2.83-2.83L9 14.77l9.88-9.89l2.83 2.83z"/></svg>';
